537ff4c6a7a2547af59a08f45b3f39bff5a6015f
ayydarsh <aadarsh2002@icloud.com>
2023-12-19 19:09:20 -0500

    added necessary scripts and files

---
+++ pages/sidebar.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.sidebar item').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
